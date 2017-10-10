fca878a781ddf7dbeceab8fa857cc7d1136cbc29
sreekanth1319 <chundrusreekanth1319@gmail.com>
2017-10-10 07:23:43 -0400

    numbers

--- Numbers/numbers.js
+++ Numbers/numbers.js
@@ -1,9 +1,16 @@
 function rndVal(){
-	var min=document.getElementById("minVal").value;
-	var max=document.getElementById("maxVal").value;
-	(function randomVal(min,max){
-		console.log(min);
-		var x=Math.random() * (max - min) + min;
+	var min=parseInt(document.getElementById("minVal").value,10);
+	var max=parseInt(document.getElementById("maxVal").value,10);
+	var x= "";
+	console.log("My X"+x);
+	(function(){
+		x=Math.random()*(max - min)+min;
 		document.getElementById("rndout").innerHTML=x;
-})();
-}//ask
+})();//closure
+	var sky=Math.ceil(x);
+	document.getElementById("topEle").innerHTML="ceil method:"+sky;
+	var ground=Math.floor(x);
+	document.getElementById("downEle").innerHTML="floor method:"+ground;
+	
+	
+}
