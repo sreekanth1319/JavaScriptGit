e27e3d65431ea2ade75281d1852d86e427fd7bf5
sreekanth1319 <chundrusreekanth1319@gmail.com>
2017-10-10 08:45:52 -0400

    strings second

--- Strings/strings.js
+++ Strings/strings.js
@@ -2,15 +2,16 @@ var z;
 function addStr(){
 	var first=document.getElementById("firstVar").value;
 	var second=document.getElementById("secondVar").value;
-	var z=first.concat(second);
+	z=first.concat(second);
 	document.getElementById("add").innerHTML=z;
 };
-/*function cut(){
+function cut(){
 	console.log(z);
 	var a= z.substring(3,4);
 	document.getElementById("cutit").innerHTML=a;
-};*/
-/*function upperCase() {
-    var z= document.getElementById("add").innerHTML;
+};
+function up() {
+    z= document.getElementById("add").innerHTML;
+	console.log(z);
     document.getElementById("upit").innerHTML = z.toUpperCase();
-}*/
+}
